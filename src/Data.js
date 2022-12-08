ebae6e9a3ce720b837c4a1edb6b38433db7df838
Tyrk <tomasdenatale@gmail.com>
2022-12-07 21:23:40 -0300

    Primera Version

---
+++ src/Data.js
@@ -0,0 +1,28 @@
+let shopItemsData = [{
+  id: "cpu",
+  name: "Procesador",
+  price: 10000,
+  desc: "Amd Ryzen 5 7600X 5.3 Ghz - AM5 Sin Cooler",
+  img: "https://mexx-img-2019.s3.amazonaws.com/Procesador-Amd-Ryzen-5-7600X-AM5-Sin-Cooler_44152_1.jpeg"
+},
+{
+  id: "GPU",
+  name: "Placa de Video",
+  price: 20000,
+  desc: "Radeon RX 6700 XT 12Gb PowerColor Red Devil Oc",
+  img: "https://mexx-img-2019.s3.amazonaws.com/40699_1.jpeg"
+},
+{
+  id: "ram",
+  name: "Memoria Ram DDR4",
+  price: 4000,
+  desc: "8Gb 2666 Mhz Kingston Fury Beast Rgb",
+  img: "https://mexx-img-2019.s3.amazonaws.com/40454_1.jpeg"
+},
+{
+  id: "mb",
+  name: "Placa Madre",
+  price: 10000,
+  desc: "AM5 - Msi PRO X670-P WIFI",
+  img: "https://mexx-img-2019.s3.amazonaws.com/Motherboard-AM5-Msi-PRO-X670-P-WIFI_44321_1.jpeg"
+}];
